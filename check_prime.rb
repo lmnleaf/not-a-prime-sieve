0c8bb26f401d05545ce53cbf84f4adb50e90dbba
lmnleaf <lmnleaf@users.noreply.github.com>
2024-03-24 10:37:28 -0600

    refactor and add command line interaction

---
+++ check_prime.rb
@@ -0,0 +1,38 @@
+require_relative 'prime_number_checker.rb'
+
+welcome_message = "This is a prime number checker. \n
+  It works kind of like a prime sieve, 
+  but it will NOT tell you all the primes smaller than a given number. 
+  Instead, it will tell you if a number is prime. \n
+  Note: This prime number checker is written in Ruby, 
+  so it will take a LONG time to complete, if the number you provide is very big. \n
+  What number do you want to check?"
+
+puts welcome_message
+
+number = gets.chomp.to_i
+
+puts "Do you want to view start and end time to get a sense of how long the calculation takes?"
+
+track_time = gets.chomp.downcase
+
+attempts = 0
+track_time = nil
+
+while attempts < 3 && (track_time != true && track_time != false)
+  if track_time == 'yes'
+    track_time = true
+  elsif track_time == 'no' || track_time == nil
+    track_time = false
+  else
+    puts "Do you want to see view start and end time
+      to get a sense of how long the calculation takes?
+      Please enter yes or no."
+    attempts += 1
+    track_time = false
+  end
+end
+
+checker = PrimeNumberChecker.new(number, track_time)
+checker.check_number
+
---
+++ prime_number_checker.rb
@@ -0,0 +1,98 @@
+class PrimeNumberChecker
+  def initialize number, track_time
+    @number = number
+    @track_time = track_time
+  end
+
+  attr_accessor :number, :prime, :prime_factor, :elapsed_time, :track_time
+
+  def check_number
+    if track_time
+      prime_check_with_time_calculation
+    else
+      prime_check
+    end
+  
+    puts "#{result}"
+  end
+
+  private
+
+  def prime_check
+    return true if number == 2 || number == 3
+  
+    self.prime = false
+  
+    digits = number.digits
+    units = digits.first
+  
+    if [0, 2, 4, 6, 8].include?(units)
+      self.prime_factor = 2
+      return
+    end
+  
+    if units == 5
+      self.prime_factor = 5
+      return
+    end
+  
+    sum = digits.inject(0){ |sum, x| sum + x }
+    if passesFirstChecks == false && sum % 3 == 0
+      self.prime_factor = 3
+      return
+    end
+  
+    # Failsd First Checks
+    # set the values for the starting equation
+    # 2((n - 3)/2) + 3 = n
+    # 2(n - 3)/2) - 3) + 2(3) + 3 = n
+    # 2((n - 3/2) - 3) + 3(3) = n
+    # 2(a) + 3(b) = n
+    r = number - 3
+    a = (r/2 - 3)
+    b = 3
+
+    prime = false
+
+    while a >= b/2
+      if a % b != 0
+        prime = true
+        a -= 3
+        b += 2
+      else
+        prime = false
+        self.prime_factor = (a/b * 2) + 3
+        break
+      end
+    end
+  end
+
+  def prime_check_with_time_calculation
+    start_time = Process.clock_gettime(Process::CLOCK_REALTIME)
+
+    prime_check
+
+    end_time = Process.clock_gettime(Process::CLOCK_REALTIME)
+
+    start_min = end_time.to_f * 1000
+    end_min = end_time.to_f * 1000
+
+    elapsed_time = end_min - start
+  end
+
+  def result
+    output = if prime == true
+      "#{number} is prime"
+    else
+      "#{number} is NOT prime \n
+      Prime factor: #{prime_factor}"
+    end
+
+    if track_time
+      output = output + 
+        "\n Elapsed Time: #{elapsed_time}"
+    end
+
+    output
+  end
+end
