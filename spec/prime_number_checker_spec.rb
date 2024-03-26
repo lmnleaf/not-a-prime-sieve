74dbaadae6d38bde82f01c1a8c76a257f12e90cc
lmnleaf <lmnleaf@users.noreply.github.com>
2024-03-26 16:30:07 -0600

    refactors PrimeNumberChecker

--- spec/prime_number_checker_spec.rb
+++ spec/prime_number_checker_spec.rb
@@ -1,68 +1,103 @@
 require_relative 'spec_helper.rb'
 
 RSpec.describe 'PrimeNumberChecker' do
-  let (:checker) { PrimeNumberChecker.new(number, track_time) }
-  let (:number) { 49 }
-  let (:track_time) { false }
+  let (:checker) { PrimeNumberChecker.new(test_number) }
+  let (:test_number) { 2 }
 
-  it 'returns a statement indicating whether a number is prime' do
-    expect(checker.check_number).to eq(
-      "#{number} is NOT prime \n" + 
-      "Prime factor: 7"
-    )
+  describe 'easy primes' do
+    describe 'when the number is 2' do
+      it 'outputs prime and no prime factors' do
+        expect(checker.check_prime).to eq({ prime: true, prime_factor: nil })
+      end
+
+      it 'does not attempt to check for additional prime factors' do
+        expect(checker).not_to receive(:easy_prime_factors?)
+        expect(checker).not_to receive(:other_prime_factors?)
+        checker.check_prime
+      end
+    end
+
+    describe 'when the number is 3' do
+      let (:test_number) { 3 }
+
+      it 'outputs prime and no prime factors' do
+        expect(checker.check_prime).to eq({ prime: true, prime_factor: nil })
+      end
+
+      it 'does not attempt to check for additional prime factors' do
+        expect(checker).not_to receive(:easy_prime_factors?)
+        expect(checker).not_to receive(:other_prime_factors?)
+        checker.check_prime
+      end
+    end
   end
 
-  describe 'easy cases' do
+  describe 'easy prime factors' do
     describe 'when the number is divisible by 2' do
-      let (:number) { 24 }
+      let (:test_number) { 24 }
 
-      it 'sets the prime factor to 2' do
-        expect(checker).not_to receive(:check_any_prime_factors)
-        expect(checker.check_number).to eq(
-          "#{number} is NOT prime \n" + 
-          "Prime factor: 2"
-        )
+      it 'outputs a prime factor and NOT prime' do
+        expect(checker.check_prime).to eq({ prime: false, prime_factor: 2 })
+      end
+
+      it 'does not attempt to check for additional prime factors' do
+        expect(checker).not_to receive(:other_prime_factors?)
+        checker.check_prime
       end
     end
   
     describe 'when the number is divisible by 3' do
-      let (:number) { 333 }
+      let (:test_number) { 333 }
 
-      it 'sets the prime factor to 3' do
-        expect(checker).not_to receive(:check_any_prime_factors)
-        expect(checker.check_number).to eq(
-          "#{number} is NOT prime \n" + 
-          "Prime factor: 3"
-        )
+      it 'outputs a prime factor and NOT prime' do
+        expect(checker.check_prime).to eq({ prime: false, prime_factor: 3 })
+      end
+
+      it 'does not attempt to check for additional prime factors' do
+        expect(checker).not_to receive(:other_prime_factors?)
+        checker.check_prime
       end
     end
 
     describe 'when the number is divisible by 5' do
-      let(:number) { 25 }
+      let(:test_number) { 25 }
 
-      it 'sets the prime factor to 5' do
-        expect(checker).not_to receive(:check_any_prime_factors)
-        expect(checker.check_number).to eq(
-          "#{number} is NOT prime \n" + 
-          "Prime factor: 5"
-        )
+      it 'outputs a prime factor and NOT prime' do
+        expect(checker.check_prime).to eq({ prime: false, prime_factor: 5 })
+      end
+
+      it 'does not attempt to check for additional prime factors' do
+        expect(checker).not_to receive(:other_prime_factors?)
+        checker.check_prime
       end
     end
   end
 
   describe 'harder cases' do
-    let (:number) { 77 }
+    describe 'when the number is NOT prime' do
+      let (:test_number) { 77 }
 
-    it 'sets the prime factor to another prime when the number is divisible by something other than 2, 3, or 5' do
-      expect(checker).to receive(:check_any_prime_factors)
-      checker.check_number
+      it 'outputs a prime factor and NOT prime' do
+        expect(checker.check_prime).to eq({ prime: false, prime_factor: 11 })
+      end
+
+      it 'checks for additional prime factors' do
+        expect(checker).to receive(:other_prime_factors?)
+        checker.check_prime
+      end
     end
 
-    it 'sets the prime factor to 7' do
-      expect(checker.check_number).to eq(
-        "#{number} is NOT prime \n" + 
-        "Prime factor: 11"
-      )
+    describe 'when the number is prime' do
+      let(:test_number) { 101 }
+
+      it 'outputs prime and does not set any prime factors' do
+        expect(checker.check_prime).to eq({ prime: true, prime_factor: nil })
+      end
+
+      it 'checks for additional prime factors' do
+        expect(checker).to receive(:other_prime_factors?)
+        checker.check_prime
+      end
     end
   end
 end
